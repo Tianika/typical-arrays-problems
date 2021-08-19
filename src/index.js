0fac6bc6569a2ddef4402be8ab5759d653b7a03c
Tianika <tatyriana@yandex.ru>
2021-08-19 19:25:40 +0300

    task complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
+exports.min = function min(array) {
+    if (array) {
+        return array.length > 0 ? array.sort((a, b) => a - b)[0] : 0;
+    }
+    return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array) {
+        return array.length > 0 ? array.sort((a, b) => b - a)[0] : 0;
+    }
+    return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array) {
+        return array.length > 0
+            ? array.reduce((sum, item) => sum + item) / array.length
+            : 0;
+    }
+    return 0;
+};
